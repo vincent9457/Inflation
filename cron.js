16ace99de471021a408b76174bfe4b4c505b94d8
vincent9457 <26395137vin@gmail.com>
2025-06-11 13:20:51 +0800

    新增自動爬蟲功能

--- cron.js
+++ cron.js
@@ -1,8 +1,7 @@
 const cron = require('node-cron');
 const { exec } = require('child_process');
 
-// 每天凌晨 5 點自動執行一次爬蟲
-cron.schedule('0 5 * * *', () => {
+cron.schedule('0 */8 * * *', () => {
     console.log('開始自動更新雞蛋價格資料...');
     exec('node fetchEggPrice.js', (err, stdout, stderr) => {
         if (err) {
@@ -13,5 +12,4 @@ cron.schedule('0 5 * * *', () => {
     });
 });
 
-// 讓這個腳本持續運行
-console.log('排程自動更新啟動中（每天凌晨5點）...');
+console.log('排程自動更新啟動中...');
